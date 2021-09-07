21972ff54e2dd8c044ef90e1c80bb08e13f30830
Sarvesh Panday <sarveshpanday@gmail.com>
2021-09-07 15:10:27 -0400

    added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi There! How are you!
+          Learn React
         </a>
       </header>
     </div>
